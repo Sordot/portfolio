c9001df7d5d43a378e0861d0a542386460acb5dc
Sordot <tgevirtz@gmail.com>
2022-08-12 12:38:01 -0500

    first commit

---
+++ src/app/about/skill-carousel/skill-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-carousel',
+  templateUrl: './skill-carousel.component.html',
+  styleUrls: ['./skill-carousel.component.scss']
+})
+export class SkillCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillCarouselComponent } from './about/skill-carousel/skill-carousel.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectsComponent,
+    SkillCarouselComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
